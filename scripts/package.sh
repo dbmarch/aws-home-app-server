ff477d0c04f1315ac6c2f800e04715fcc07635ab
dbmarch <dbmarch@gmail.com>
2019-04-22 18:45:06 -0500

    fix paths

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,3 @@
-VERSION=`node -e "console.log(require('./package.json').version);"`
-APP_NAME=`node e "console.log(require('./package.json').name);"`
+VERSION=`node -e "console.log(require('../package.json').version);"`
+APP_NAME=`node e "console.log(require('../package.json').name);"`
 zip -r ${APP_NAME}-${VERSION}.zip lib node_modules
