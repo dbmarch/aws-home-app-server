cd706dd933f6f5e067571baa96252cd3c2f3560b
dbmarch <dbmarch@gmail.com>
2019-04-23 06:26:45 -0500

    see if build passes when we comment out aws command

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -38,7 +38,7 @@ docker push $AWS_REGISTRY/$ECR_REPO:latest || exit 1
 
 # Update the Cloud formation Template
 echo "STACK NAME: "${STACK_NAME}
-export STACK_INFO=`aws cloudformation describe-stacks --stack-name $STACK_NAME`
+#export STACK_INFO=`aws cloudformation describe-stacks --stack-name $STACK_NAME`
 STACK_PARAMETERS=`node -e"console.log(JSON.stringify(JSON.parse(process.env.STACK_INFO).Stacks[0].Parameters))"`
 STACK_STATUS=`node -e "console.log(JSON.parse(process.env.STACK_INFO).Stacks[0].StackStatus"`
 
